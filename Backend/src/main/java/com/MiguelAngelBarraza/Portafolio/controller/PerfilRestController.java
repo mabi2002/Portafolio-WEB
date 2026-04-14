f25905607a4040339f8436dcb165fd829f2fa427
mabi2002 <miguel_abl@outlook.com>
2026-04-14 13:04:01 -0700

    fix: Remove hardcoded @CrossOrigin annotations to use global CORS configuration

--- Backend/src/main/java/com/MiguelAngelBarraza/Portafolio/controller/PerfilRestController.java
+++ Backend/src/main/java/com/MiguelAngelBarraza/Portafolio/controller/PerfilRestController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/perfil")
-@CrossOrigin(origins = "http://localhost:4200")
 public class PerfilRestController {
 
     @Autowired
